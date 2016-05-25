c44071b95c57aed201c06b8519472e20aaaaf85c
speaker73 <speaker73@ukr.net>
2016-05-25 20:56:04 +0300

    Create seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,7 +14,43 @@ class DatabaseSeeder extends Seeder {
 	{
 		Model::unguard();
 
-		// $this->call('UserTableSeeder');
+		 $this->call('CompanyTableSeeder');
+	}
+
+
+}
+class CompanyTableSeeder extends Seeder {
+
+	public function run()
+	{
+		DB::table('companies')->insert([
+			'id' => 1,
+			'name' => 'Company1',
+			'amount' => 25,
+			'total_amount' => 53,
+			'parent' => 1
+		]);
+		DB::table('companies')->insert([
+			'id' => 2,
+			'name' => 'Company2',
+			'amount' => 13,
+			'total_amount' => 18,
+			'parent' => 1
+		]);
+		DB::table('companies')->insert([
+			'id' => 3,
+			'name' => 'Company3',
+			'amount' => 5,
+			'total_amount' => 5,
+			'parent' => 2
+		]);
+		DB::table('companies')->insert([
+			'id' => 4,
+			'name' => 'Company4',
+			'amount' => 10,
+			'total_amount' => 10,
+			'parent' => 1
+		]);
 	}
 
 }
