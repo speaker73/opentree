9bf5d210fff7d312d6116407dc5cd71bdb67fca1
speaker73 <speaker73@ukr.net>
2016-05-25 20:43:19 +0300

    Create table

---
+++ database/migrations/2016_05_25_173454_create_companies_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCompaniesTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('companies', function(Blueprint $table)
+		{
+			$table->increments('increment');
+			$table->integer('id');
+			$table->integer('parent');
+			$table->string('name');
+			$table->float('amount');
+			$table->float('total_amount')->nullable();
+			$table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('companies');
+	}
+
+}
