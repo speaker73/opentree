2dd9cb1f86b2e0890028f912912fc85e2cfbfac1
speaker73 <speaker73@ukr.net>
2016-05-26 06:13:06 +0300

    create js tree builder and calc

--- resources/views/tree.blade.php
+++ resources/views/tree.blade.php
@@ -11,6 +11,20 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css"
           integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
     {{--<link rel="stylesheet" href="bower_resources/bootstrap/dist/css/bootstrap-theme.css">--}}
+    <style>
+        .ul-layer
+        {
+            background-color: #fff;
+            -webkit-background-clip: padding-box;
+            background-clip: padding-box;
+            border: 1px solid #ccc;
+            border: 1px solid rgba(0,0,0,.15);
+            border-radius: 4px;
+            -webkit-box-shadow: 0 6px 12px rgba(0,0,0,.175);
+            box-shadow: 0 6px 12px rgba(0,0,0,.175);
+            padding-bottom: 20px;
+        }
+    </style>
 </head>
 <body>
 
@@ -19,7 +33,7 @@
         <ul class="list-group center-block" id="tree">
             @foreach($companies as $item)
 
-                <li id="{{$item->id}}" class="list-group-item h3 panel panel-success alert-info" data-parent="{{$item->parent}}"
+                <li id="{{$item->id}}" class="list-group-item h3 panel-success alert-info" data-parent="{{$item->parent}}"
                     data-amount="{{$item->amount}}" data-total_amount="{{$item->amount}}" data-level="" >
 
 
@@ -46,14 +60,7 @@
 
 
                 </li>
-                {!!"<ul style='background-color: #fff;
-    -webkit-background-clip: padding-box;
-    background-clip: padding-box;
-    border: 1px solid #ccc;
-    border: 1px solid rgba(0,0,0,.15);
-    border-radius: 4px;
-    -webkit-box-shadow: 0 6px 12px rgba(0,0,0,.175);
-    box-shadow: 0 6px 12px rgba(0,0,0,.175);' id='ul".$item->id."'></ul>"!!}
+                {!!"<ul class='ul-layer' id='ul".$item->id."'></ul>"!!}
                 {{--{!!$item->parent == 2?"</ul>":""!!}--}}
             @endforeach
         </ul>
@@ -163,104 +170,7 @@
         @foreach($companies as $item)
                 tree_arr[tree_arr.length] = {{$item->id}};
         @endforeach
-        /*console.log(tree_arr);*/
-        function tree_builder() {
-            for (var i = 0; i <= li_length; i++) {
-                var parent = $('#' + tree_arr[i]).attr('data-parent');
-                if (tree_arr[i] != parent) {
-                    $("#" + tree_arr[i])
-                            .appendTo("#ul" + parent);
-                    $("#ul" + tree_arr[i])
-                            .appendTo("#ul" + parent);
-                }
-            }
-        }
-        tree_builder();
-        /*Edit form*/
-        function add_value() {
-            var number = null;
-            if (li_length == 0) {
-                number = 1;
-            } else {
-                number = tree_arr[tree_arr.length - 1] + 1;
-            }
-            $("#input_parent").attr("value", number);
-            $("#input_id").attr("value", number);
-        }
-        add_value();
-        /* Calculate total amount  */
-        function total_calc(id) {
-            var our_amount = Number($('#' + id).attr('data-amount'));
-            var total_amount_id = our_amount;
-            var our_parent = id;
-            for (var i = 0; i < li_length; i++) {
-                if (tree_arr[i] != our_parent) {
-                    var id_parent = $('#' + tree_arr[i]).attr('data-parent');
-                    /*console.log('id:'+id_parent);*/
-                    var id_amount = Number($('#' + tree_arr[i]).attr('data-total_amount'));
-                    /*console.log(id_amount);*/
-                    if (id_parent == our_parent) {
-                        total_amount_id = Number(total_amount_id + id_amount);
-                    }
-                }
-            }
-            return total_amount_id;
-        }
-        /* Add total amount from tree*/
-        function total_add(i) {
-            $("#" + i+" .panel-footer").append("<label role='presentation' class='label label-primary'>Total  <span class='badge' style='color:#337AB7;background:#fff;'>$" + total_calc(i) + "K</span></label>");
-            $("#" + i).attr("data-total_amount", total_calc(i));
-        }
-        /*The order of addition*/
-
-        var level_arr = [];
-        function order_add_level() {
-            for (var i = 0; i < tree_arr.length; i++) {
-                var our_parent = $('#' + tree_arr[i]).attr('data-parent');
-                var our_parent_length = $('.list-group-item[data-parent="' + tree_arr[i] + '"]').length;
-                if (our_parent == tree_arr[i]) {
-                    $("#" + tree_arr[i]).attr("data-level", 1);
-                    /*tree_arr.splice(i, 1);*/
-                    level_arr[level_arr.length] = 1;
-                }
-                if (our_parent != tree_arr[i]) {
-                    $("#" + tree_arr[i]).attr("data-level", 2);
-                    /*tree_arr.splice(i, 1);*/
-                    level_arr[level_arr.length] = 2;
-                }
-                var id_parent_level = Number($('#' + our_parent).attr('data-level'));
-                if (id_parent_level > 1) {
-                    $("#" + tree_arr[i]).attr("data-level", id_parent_level + 1);
-                    level_arr.splice(level_arr.length - 1, 1);
-                    level_arr[level_arr.length] = id_parent_level + 1;
-                }
-            }
-        }
-        order_add_level();
-        function sortDown(a, b) {
-            if (a < b) return 1;
-            if (a > b) return -1;
-        }
-        level_arr.sort(sortDown);
-        var max_level = Math.max.apply(Math, level_arr);
-        function order_calc() {
-            console.log("level_arr:[" + level_arr + "]");
-            console.log("tree_arr:[" + tree_arr + "]");
-            for (var i = max_level; i > 0; i--) {
-                for (var j = 0; j < level_arr.length; j++) {
-                    var level = i;
-                    var id_level = Number($('#' + tree_arr[j]).attr('data-level'));
-                    console.log("level:" + i + " level_id: " + id_level + " id:" + tree_arr[j]);
-                    if (level == id_level) {
-                        console.log(level + " Print it! " + tree_arr[j]);
-                        total_add(tree_arr[j]);
-
-                    }
-                }
-            }
-        }
-        order_calc();
     </script>
-
+    <script src="opentree.js"></script>
 </body>
 </html>
